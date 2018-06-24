decd53295bddb55fdefc37385eddf42b2c07a242
Gunta Chomchalerm <guntachomchalerm@Guntas-MacBook-Pro.local>
2018-06-24 19:27:58 +0700

    split name

--- test_dataclean.py
+++ test_dataclean.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
-
 import numpy as np 
 import pandas as pd 
 
+#read data from test.csv
 df = pd.read_csv('test.csv')
+print('-----test.csv-----')
 print(df.head(3))
 
+df['Last Name'], df['First Name'] = df['Name'].str.split(',', 1).str
 
+#write data to test_clean.csv
+df.to_csv('test_processed.csv', encoding='UTF-8', index=False) 
+print('-----test_processed.csv-----')
+print(df)
+print(df.head(3))
